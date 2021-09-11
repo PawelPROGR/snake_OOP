cc6cd69d72ca92bd78c2be61d2c57cb7f55fb626
pawel <pawel.2020.navtop@gmail.com>
2021-09-11 17:59:24 +0300

    v-0-2

---
+++ snake_OOP/Point.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace snake_OOP
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char zv;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(zv);
+        }
+
+        public int rand()
+        {
+            int x;
+            Random rnd = new Random();
+            x = rnd.Next(0, 20);
+            return x;
+        }
+    }
+}
--- snake_OOP/Program.cs
+++ snake_OOP/Program.cs
@@ -6,33 +6,19 @@ namespace snake_OOP
     {
         static void Main(string[] args)
         {
-            int x1, y1, x2, y2;
-            char zv = '*';
+            Point p1 = new Point();
+            p1.x = p1.rand();
+            p1.y = p1.rand();
+            p1.zv = '*';
+            p1.Draw();
 
-            x1 = rand();
-            y1 = rand();
-
-            Draw(x1, y1, zv);
-
-            x2 = rand();
-            y2 = rand();
-            Draw(x2, y2, zv);
+            Point p2 = new Point();
+            p2.x = p1.rand();
+            p2.y = p1.rand();
+            p2.zv = '*';
+            p2.Draw();
 
             Console.ReadLine();
-        }
-
-        static void Draw(int x, int y, char zv)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(zv);
-        }
-
-        static int rand()
-        {
-            int x;
-            Random rnd = new Random();
-            x = rnd.Next(0, 20);
-            return x;
         }
     }
 }

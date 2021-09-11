e95da223f4ec33c49879011176c1b8c51a8396c0
pawel <pawel.2020.navtop@gmail.com>
2021-09-11 16:57:15 +0300

    v-0-1

--- snake_OOP/Program.cs
+++ snake_OOP/Program.cs
@@ -6,8 +6,33 @@ namespace snake_OOP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x1, y1, x2, y2;
+            char zv = '*';
+
+            x1 = rand();
+            y1 = rand();
+
+            Draw(x1, y1, zv);
+
+            x2 = rand();
+            y2 = rand();
+            Draw(x2, y2, zv);
+
             Console.ReadLine();
+        }
+
+        static void Draw(int x, int y, char zv)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(zv);
+        }
+
+        static int rand()
+        {
+            int x;
+            Random rnd = new Random();
+            x = rnd.Next(0, 20);
+            return x;
         }
     }
 }
